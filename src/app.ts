c00fb275b182892ca8c0c89227b6636c8887feba
khalid <khankhalid1743@gmail.com>
2023-11-07 11:13:57 +0530

    Setup Project

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express';
+
+import { startClient } from './setupServer';
+
+import databaseConnection from './setupDatabase';
+
+import { config } from './config';
+
+function initialize(): void {
+  loadConfig();
+  databaseConnection();
+  const app: Express = express();
+  startClient(app);
+}
+
+function loadConfig(): void {
+  config.validateConfig();
+}
+
+initialize();
